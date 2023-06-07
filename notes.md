e27c9bf35db340f19d8a7744069218a5abc423d4
Corin Baurmann <corin.baurmann@gmail.com>
2023-06-08 01:15:44 +0200

    aggregate poverty and population, first plot

--- notes.md
+++ notes.md
@@ -5,6 +5,11 @@
 542 Kinder/Lor, XLSX von Statistikamt berlin brandenburg, erste Hälfte 2021
 542 kinderarmut prozent/lor, gescraped von FIS, 2020 12
 
+## childpoverty
+543 disctricts
+6 invalid (below 300 inhabitants)
+december 2020
+
 ## Playgrounds
 1873 playgrounds
     CSV von FIS, 
@@ -15,6 +20,7 @@
         objektnummer: 40232
         ort: jenneweg, spandau
 1797 accreditable as playground
+data from 01.03.2023
 
 ## LOR x Playground
 
@@ -26,6 +32,11 @@
 ## Playgrounds
 - some playgrounds do not seem to be in the cities registry
 - parks are not included in the analysis and might be better than a playground
+- not all playgrounds had effective playarea
 
 ## LOR
 - borders arbitrary, playgrounds within as well
+    - mitigation by calculating adjunctness
+
+## children
+- poverty data from 2020/12, population from 2021/6
--- scripts/aggregate_lors.py
+++ scripts/aggregate_lors.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib as mpl
 
-lors = pd.read_csv('../assets/lor_2021.csv', sep=';', index_col='PLR_id')
-children = pd.read_csv('../assets/children_06_2022.csv', sep=',', index_col='PLR')
+sns.set_theme()
+
+lors = pd.read_csv('../assets/lor_2021.csv', sep=';', index_col='PLR_id', usecols=['PLR_id', 'PLR_name', 'area_sqm'])
+childpoverty= pd.read_csv('../assets/childpoverty_202012.csv', sep='|', index_col='PLR_id', usecols=['PLR_id', 'poverty', 'comment'])
+population = pd.read_csv('../assets/children_06_2021.csv', sep=',', encoding='utf-8', index_col='PLR_id')
 lors_playgrounds = pd.read_csv('../assets/lor_playgrounds.csv', sep=',', encoding='utf-8', index_col='PLR_id')
-playgrounds = pd.read_excel('../assets/playgrounds.xls', index_col='id')
+playgrounds = pd.read_excel('../assets/playgrounds.xls', index_col='id', usecols=['id', 'anrechenbar Spielplatzversorgung', 'Nettospielfläche in m²', 'Größe in m² (Kataster)'])
 
 # drop non accredible playgrounds, set total area for playable area for missing values
 playgrounds = playgrounds[playgrounds['anrechenbar Spielplatzversorgung']].drop(['anrechenbar Spielplatzversorgung'], axis=1)
@@ -12,16 +17,31 @@ playgrounds['playground_area'] = playgrounds.apply(lambda row: row['Größe in m
 playgrounds.drop(['Größe in m² (Kataster)', 'Nettospielfläche in m²'], axis=1, inplace=True)
 playgrounds.index.names = ['playground_id']
 
-print(playgrounds)
+# drop invalid districts for childpoverty, change datatype for poverty
+childpoverty = childpoverty[childpoverty['comment'] == 'gültig'].drop(['comment'], axis=1)
+childpoverty['poverty'] = childpoverty['poverty'].str.replace(',', '.').astype(float)
 
-lors_playgrounds = lors_playgrounds.join(playgrounds, on='playground_id').drop('PLR_id', axis=1)
+
+# aggregate children
+population['total_children'] = population['<6'] + population['6-15'] + population['15-18']
+
+lors_playgrounds = lors_playgrounds.join(playgrounds, on='playground_id')
 
 # count playgrounds within Lor
 agg_funcs = {'playground_id': 'count', 'playground_area': 'sum'}
 lors[['playground_count', 'playground_area']] = lors_playgrounds[['playground_id', 'playground_area']].groupby('PLR_id').aggregate(agg_funcs)
 
-# if no association could be made, row is NaN, fill with 0 playgroudds
+# if no association could be made, row is NaN, fill with 0 playgrounds respective area
 lors['playground_count'].fillna(0, inplace=True)
 lors['playground_area'].fillna(0, inplace=True)
 
-print(lors)
+childpoverty = childpoverty.join(population[['total_population', 'total_children']])
+#childpoverty['poor_children'] = childpoverty['total_children'] * childpoverty['poverty'] / 100
+
+lors = lors.join(childpoverty)
+
+lors['pl_area_per_child'] = lors['playground_area'] / lors['total_children']
+#lors = lors.loc[lors['pl_area_per_child']!=lors['pl_area_per_child'].max()]
+
+sns.relplot(lors, x='playground_count', y='poverty')
+mpl.pyplot.show()
