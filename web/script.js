fb7caa131a50c8120b4994cc7907a552649c9c08
Corin Baurmann <corin.baurmann@gmail.com>
2023-06-09 00:36:22 +0200

    fix small mistakes, link assets to dropbox

--- web/script.js
+++ web/script.js
@@ -9,29 +9,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let fetch_LORs = async () => {
 
-    let response = await fetch('https://dl.dropbox.com/s/vrb9cc5pv12rj17/lor_2021_rewound.geojson?dl=1')
+    let response = await fetch('https://dl.dropbox.com/s/6cbejuci59llu70/lors_2021_rewound_amended.geojson?dl=1')
         .then(res => res.text());
     
     let LORs = JSON.parse(response);
 
-    response = await fetch('https://dl.dropbox.com/s/ho9co34uutkxqt3/playgrounds_wfs_rewound_transformed.geojson?dl=0')
+    response = await fetch('https://dl.dropbox.com/s/31qo9aa4g1yi5yj/playgrounds_wfs_rewound_transformed.geojson?dl=1')
     .then(res => res.text());
 
     let playgrounds = JSON.parse(response);
 
-    let [min, max] = get_max_min(LORs);
-
-    console.log(min,max)
-
     L.geoJSON(LORs, {
         style: function(feature) {
             return {
-                fillColor: get_color(feature.properties.GROESSE_M2, min, max),
+                fillColor: get_color(feature.properties.pl_area_per_child),
                 weight: 2,
                 opacity: 1,
                 color: 'black',
                 dashArray: '3',
-                fillOpacity: 0.3
+                fillOpacity: 0.6
             };
         }
     }).addTo(map);
@@ -40,36 +36,17 @@ let fetch_LORs = async () => {
 
 }
 
-function get_color(size, min, max) {
-    let grade = (max - min)/6;
+function get_color(value) {
 
-    return  size > 5*grade ? '#bd0026' :
-    size > 4*grade  ? '#f03b20' :
-    size > 3*grade  ? '#fd8d3c' :
-    size > 2*grade   ? '#feb24c' :
-    size > grade   ? '#FED976' :
-               '#ffffb2';
-
-}
-
-function get_max_min(geoJSON) {
-
-    let min = geoJSON.features[0].properties.GROESSE_M2;
-    let max = geoJSON.features[0].properties.GROESSE_M2;
-
-    for (feature of geoJSON.features) {
-
-        let val = feature.properties.GROESSE_M2;
-
-        if (val > max) {
-            max = val;
-        } else if (val < min) {
-            min = val
-        }
+    if (value>6.24) return '#ffffb2'
+    else if (value>4.14) return '#FED976'
+    else if (value>2.85) return '#feb24c'
+    else if (value>1.55) return '#fd8d3c'
+    else if (value>0) return '#f03b20'
+    else return '#808080'
     
-    }
 
-    return [min, max]
 }
+
 
 fetch_LORs();
